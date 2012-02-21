a00fde1769004f87eeff70627993d574fae21e4b
Jonathan Dray <jonthan.dray@gmail.com>
2012-02-21 11:14:20 +0100

    Fix : extraction de l'url du service à appeler : doit maintenant contenir /api/jmi dans le path.

--- wordpress-web/src/main/java/com/socialcomputing/wordpress/utils/URLUtil.java
+++ wordpress-web/src/main/java/com/socialcomputing/wordpress/utils/URLUtil.java
@@ -5,6 +5,8 @@ package com.socialcomputing.wordpress.utils;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +16,9 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class URLUtil {
+	public static final Pattern WORDPRESS_PATH_REGEXP = Pattern.compile("([-a-zA-Z0-9+&@#/%=~_|]*/api/jmi)(.*)");
 	private static final Logger LOG = LoggerFactory.getLogger(URLUtil.class);
+	
 
 	public URLUtil() {
 		throw new UnsupportedOperationException();
@@ -28,11 +32,12 @@ public class URLUtil {
     public static final String normalizeUrl(String url) throws MalformedURLException {
         URL u = new URL(url);
         int port = u.getPort();
-        String[] pathElements = u.getPath().substring(1).split("/");
-        if(pathElements.length < 3) 
-        	throw new MalformedURLException("Invalid path in url, it should contain at least 3 elements: " + u.getPath());
-        String path = "/" + pathElements[0] + "/" + pathElements[1];
-        LOG.debug("path: {}", path);
+        Matcher m = WORDPRESS_PATH_REGEXP.matcher(u.getPath());
+        if(!m.find()) {
+        	throw new MalformedURLException("Invalid path in url, it should contain the /api/jmi subpath: " + u.getPath());
+        }
+        String path = m.group(1);
+        LOG.debug("extracted path: {}, last part: {}", path, m.group(2));
         return u.getProtocol() + "://" + u.getHost() + (port != -1 && port != 80 ? ":" + port : "") + path;
     }
 }
